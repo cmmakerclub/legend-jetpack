5bbbb84bebe2520e782ebcf2c7d2b47b8c7f4718
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2018-06-07 16:30:08 +0700

    add buttonModule

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,26 @@
 
 #include <CMMC_Legend.h>
 // #include <modules/KadyaiModule.h>
-// #include <modules/ESPNowModule.h>
+#include <modules/ESPNowModule.h>
 // #include <modules/OLEDModule.h>
-#include <modules/MPU6050Module.h>
+// #include <modules/MPU6050Module.h>
 
 CMMC_Legend os;
 
-// int temp = 0; // for BME680Module.cpp
+// for BME680Module.cpp
+// int temp = 0; 
 
-float axisX = 0.0;
-float axisY = 0.0;
-float axisZ = 0.0;
+// for MPU6050Module.cpp
+// float axisX = 0.0;
+// float axisY = 0.0;
+// float axisZ = 0.0;
 
 void setup()
 {
   // os.addModule(new KadyaiModule());
-  // os.addModule(new ESPNowModule());
+  os.addModule(new ESPNowModule());
   // os.addModule(new OLEDModule());
-  os.addModule(new MPU6050Module());
+  // os.addModule(new MPU6050Module());
   os.setup();
   Serial.printf("APP VERSION: %s\r\n", LEGEND_APP_VERSION);
 }
