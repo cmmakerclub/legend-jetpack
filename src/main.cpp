90e7be42301a5124c6d76f8513e6acb82a7ac260
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2018-06-12 15:17:36 +0700

    add espNOW Weight scale

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include <modules/MPU6050Module.h>
 #include <modules/PRAJModule.h>
 #include <modules/PRAJ_ESPNowModule.h>
-// #include <modules/WC_ESPNowModule.h>
+#include <modules/WC_ESPNowModule.h>
 #include <CMMC_Sensor.h>
 
 CMMC_Legend os;
@@ -21,9 +21,9 @@ void setup()
   // os.addModule(new ESPNowModule());
   // os.addModule(new OLEDModule());
   // os.addModule(new KadyaiModule());
-  os.addModule(new PRAJModule());
-  os.addModule(new PRAJ_ESPNowModule()); 
-  // os.addModule(new WC_ESPNowModule()); 
+  // os.addModule(new PRAJModule());
+  // os.addModule(new PRAJ_ESPNowModule()); 
+  os.addModule(new WC_ESPNowModule()); 
   os.setup();
   Serial.printf("APP VERSION: %s\r\n", LEGEND_APP_VERSION);
 }
