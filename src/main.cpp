84f51b035f04acd4728ea5b7274defccebf96929
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2018-06-12 11:03:26 +0700

    new esp now

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,14 @@
 #include <modules/MPU6050Module.h>
 #include <modules/PRAJModule.h>
 #include <modules/PRAJ_ESPNowModule.h>
+#include <modules/WC_ESPNowModule.h>
 #include <CMMC_Sensor.h>
 
 CMMC_Legend os;
 
-float axisX = 0.0;
-float axisY = 0.0;
-float axisZ = 0.0;
+// float axisX = 0.0;
+// float axisY = 0.0;
+// float axisZ = 0.0;
 
 void setup()
 {
@@ -20,8 +21,9 @@ void setup()
   // os.addModule(new ESPNowModule());
   // os.addModule(new OLEDModule());
   // os.addModule(new KadyaiModule());
-  os.addModule(new PRAJModule());
-  os.addModule(new PRAJ_ESPNowModule()); 
+  // os.addModule(new PRAJModule());
+  // os.addModule(new PRAJ_ESPNowModule()); 
+  os.addModule(new WC_ESPNowModule()); 
   os.setup();
   Serial.printf("APP VERSION: %s\r\n", LEGEND_APP_VERSION);
 }
