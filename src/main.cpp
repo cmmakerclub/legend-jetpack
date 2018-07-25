429b93a00cbeebe7a2ee663a818756e5c88c50eb
Nat <nat@cmmc.io>
2018-07-25 15:46:00 +0700

    update BME680.

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,15 @@
 
 #include <CMMC_Legend.h>
-#include <modules/KadyaiModule.h>
-#include <modules/ESPNowModule.h>
+#include <modules/BME680Module.h>
 #include <CMMC_Sensor.h>
 
 CMMC_Legend os;
 CMMC_SENSOR_DATA_T userKadyaiData;
 char userEspnowSensorName[16];
 
-
-// float axisX = 0.0;
-// float axisY = 0.0;
-// float axisZ = 0.0;
-
 void setup()
 {
-  // os.addModule(new KadyaiModule());
-  // os.addModule(new OLEDModule());
-  os.addModule(new KadyaiModule());
-  os.addModule(new ESPNowModule());
-  // os.addModule(new PRAJModule());
-  // os.addModule(new PRAJ_ESPNowModule()); 
-  // os.addModule(new WC_ESPNowModule()); 
-  // os.addModule(new BatteryTestESPNowModule()); 
+  os.addModule(new BME680Module());
   os.setup();
   Serial.printf("APP VERSION: %s\r\n", LEGEND_APP_VERSION);
 }
--- src/modules/BME680Module.cpp
+++ src/modules/BME680Module.cpp
@@ -1,5 +1,4 @@
 #include "BME680Module.h"
-extern int temp;
 
 CMMC_SENSOR_DATA_T data;
 void BME680Module::config(CMMC_System *os, AsyncWebServer* server) {
@@ -13,10 +12,8 @@ void BME680Module::setup() {
   bme->onData([&](void *d, size_t len) {
     Serial.printf("DATA %lu bytes ARRIVED at %lu\r\n", len, millis()); 
     memcpy(&data, d, len);
-    temp = data.field1;
-    Serial.printf("onData temp = %d\r\n", temp);
   }); 
-  bme->every(3000);
+  bme->every(1000);
   bme->setup(0, 0);
 }
 
